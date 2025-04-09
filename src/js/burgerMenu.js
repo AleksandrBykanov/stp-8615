d501583b24937e5f77bd28a95324bec7dc531802
Aleksandr Bykanov <aleksandrbykanov1986@gmail.com>
2025-04-09 12:44:37 +0300

    make modal

--- src/js/burgerMenu.js
+++ src/js/burgerMenu.js
@@ -1,1 +1,19 @@
+const burgerInput = document.querySelector('.burger-input');
+const modal = document.querySelector('.modal');
+const header = document.querySelector('.header');
+
+
+burgerInput.addEventListener('change', () => {
+  if (burgerInput.checked) {
+    modal.classList.add('active');
+    header.classList.add('transparent')
+  } else {
+    modal.classList.remove('active');
+    header.classList.remove('transparent')
+  }
+});
+
+
+
+
 
