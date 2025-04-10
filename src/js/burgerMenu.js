7093eb3af47689109e7814e16a24f7f551847adb
Aleksandr Bykanov <aleksandrbykanov1986@gmail.com>
2025-04-10 14:42:11 +0300

    fix size

--- src/js/burgerMenu.js
+++ src/js/burgerMenu.js
@@ -2,16 +2,27 @@ const burgerInput = document.querySelector('.burger-input');
 const modal = document.querySelector('.modal');
 const header = document.querySelector('.header');
 
+function checkScreenSize() {
+  if (window.innerWidth >= 1200 && modal.classList.contains('active')) {
+    modal.classList.remove('active');
+    header.classList.remove('transparent');
+    burgerInput.checked = false;
+  }
+}
 
 burgerInput.addEventListener('change', () => {
   if (burgerInput.checked) {
     modal.classList.add('active');
-    header.classList.add('transparent')
+    header.classList.add('transparent');
   } else {
     modal.classList.remove('active');
-    header.classList.remove('transparent')
+    header.classList.remove('transparent');
   }
 });
+
+checkScreenSize();
+
+window.addEventListener('resize', checkScreenSize);
 
 
 
