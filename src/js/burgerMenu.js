6c06ef0e8e497a1ae298e2de015f7872c618d3d2
Aleksandr Bykanov <aleksandrbykanov1986@gmail.com>
2025-04-13 20:54:39 +0300

    fix modal

--- src/js/burgerMenu.js
+++ src/js/burgerMenu.js
@@ -1,10 +1,12 @@
 const burgerInput = document.querySelector('.burger-input');
 const modal = document.querySelector('.modal');
+const header = document.querySelector('.header');
+const menuItems = document.querySelectorAll('.modal-item');
 
 function checkScreenSize() {
   if (window.innerWidth >= 1200 && modal.classList.contains('active')) {
     modal.classList.remove('active');
-
+    header.classList.remove('transparent');
     burgerInput.checked = false;
   }
 }
@@ -12,16 +14,21 @@ function checkScreenSize() {
 burgerInput.addEventListener('change', () => {
   if (burgerInput.checked) {
     modal.classList.add('active');
+    header.classList.add('transparent');
   } else {
     modal.classList.remove('active');
+    header.classList.remove('transparent');
   }
 });
 
-checkScreenSize();
 
-window.addEventListener('resize', checkScreenSize);
-
-
+menuItems.forEach(item => {
+  item.addEventListener('click', () => {
+    modal.classList.remove('active');
+    header.classList.remove('transparent');
+    burgerInput.checked = false; 
+  });
+});
 
 
 
